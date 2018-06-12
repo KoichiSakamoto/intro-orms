da50d875b17563f7f747fb5aab782b5a5900606a
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-06-12 14:47:12 -0400

    lecture

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ source "https://rubygems.org"
 
 gem "sqlite3"
 gem "pry"
+
+# Added at 2018-06-12 13:40:27 -0400 by graham.troyer-joy:
+gem "faker", "~> 1.8"
--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,16 +1,55 @@
 class Tweet
   attr_accessor :message, :username
 
-  ALL = []
-
   def initialize(props={})
     @message = props['message']
     @username = props['username']
-    ALL << self
+    # ALL << self
+  end
+
+  ### CREATE ###
+
+  def save_new
+    # SQL insert
+    # self
+  end
+
+  ### READ ###
+  def self.find(tweet_id)
+    sql_string = <<-SQL_STRING
+      select *
+      from tweets
+      where id = ?
+    SQL_STRING
+    array_of_hashes = DB[:conn].execute(sql_string, tweet_id)
+    Tweet.new(array_of_hashes[0])
   end
 
   def self.all
-    ALL
+    sql_string = <<-SQL_STRING
+      select *
+      from tweets
+    SQL_STRING
+
+    array_of_hashes_from_the_database = DB[:conn].execute(sql_string)
+    array_of_hashes_from_the_database.map do |hashy_tweet|
+      Tweet.new(hashy_tweet)
+    end
   end
+
+  ### UPDATE ###
+
+  def update(props={})
+    # sql update
+    # make sure we update the instance variables through accessors
+    # self
+  end
+  ### DELETE ###
+
+  def delete
+    # SQL delete
+    # true
+  end
+
 
 end
